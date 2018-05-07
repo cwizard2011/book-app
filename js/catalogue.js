457516b2508f5e035a41344a500cf4ccf809421e
Styll <stephen.aribaba@yahoo.com>
2018-05-07 15:04:12 +0100

    Combine catalogue backend and frontend

--- js/catalogue.js
+++ js/catalogue.js
@@ -12,7 +12,7 @@ min = parseInt(min);
 max = parseInt(max);
 
 
-const catalogue = document.getElementById('id1');
+const catalogue = document.getElementById('container');
 
 function getAllBooks() {
   fetch(getBookUrl).then((response) => response.json())
@@ -26,19 +26,32 @@ function getAllBooks() {
 
 function getBookObject(response) {
   const div = document.createElement('div');
-  const title = document.createElement('a');
-  const excerpt = document.createElement('h3');
+  const colMd3 = document.createElement('div');
+  colMd3.setAttribute('class','col-md-3');
+  const card = document.createElement('div');
+  card.setAttribute('class', 'card');
+  const cardBody = document.createElement('div');
+  cardBody.setAttribute('class', 'card-body')
+  const span = document.createElement('span');
+  const cardTitle = document.createElement('a');
+  cardTitle.className = 'card-title';
+  cardTitle.href = `./viewdetails.html?id=${response.ID}`;
+  const excerpt = document.createElement('p');
   const count = document.createElement('h4');
   const id = document.createElement('h5');
-  title.textContent = response.Title;
+  span.textContent = 'Book Title: ';
+  span.appendChild(cardTitle);
+  cardTitle.textContent = response.Title;
   excerpt.textContent = response.Excerpt;
   id.textContent = response.ID;
   count.textContent = response.PageCount;
-  div.appendChild(title);
-  div.appendChild(excerpt);
-  div.appendChild(id);
-  div.appendChild(count);
-  return div;
+  cardBody.appendChild(span);
+  cardBody.appendChild(excerpt);
+  // cardBody.appendChild(id);
+  // cardBody.appendChild(count);
+  card.appendChild(cardBody);
+  colMd3.appendChild(card);
+  return colMd3;
   //let content = document.createTextNode(`${wordObject.word} - ${status}`);
   //let liClass = document.createAttribute('class');
 }
